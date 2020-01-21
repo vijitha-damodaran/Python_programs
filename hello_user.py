bd3aa68f260399763b35d26410a358c9cbedefc1
vijitha-damodaran <59793003+vijitha-damodaran@users.noreply.github.com>
2020-01-21 11:27:15 +0530

    Update hello_user.py

--- hello_user.py
+++ hello_user.py
@@ -1,3 +1,26 @@
-#prompting for user name and printing Hello user name !!
-name= input ('Please enter your Name:')
-print ("Hello", name, "!!!" )
+""" 
+Prompting to enter name and Printing Hello username using function
+"""
+
+def get_username():
+    """ 
+    Prompts the user to enter his name
+    """
+    user_name =input('Enter your name:')
+    return user_name
+
+def say_hello(user):
+    """
+    Greet the user by saying Hello followed by his name
+    """
+    print("Hello",user,"!!!")
+
+def main():
+    '''
+    Main function of the program
+    '''
+    name = get_username()
+    say_hello(name)
+    
+# Main starts from below
+main()
