26a95fc425209b580b43847f77a98e1d9fdf94da
vijitha-damodaran <59793003+vijitha-damodaran@users.noreply.github.com>
2020-01-21 11:36:25 +0530

    Update mul_table_17_using_for

--- mul_table_17_use_for.py
+++ mul_table_17_use_for.py
@@ -1,6 +1,18 @@
-#Multiplication table of 17 using for
-count = int(input('Count upto which multiplication to be done: '))
-number=range(1,count+1)
-for item in number:
-    res=item*17
-    print(item,'*','17','=',res)
+""" 
+This program prints
+multiplication table of 17
+"""
+
+def print_mtable17():
+    """ 
+    Prints the multiplication table for 17
+    upto the count given by the user
+    """
+    count = int(input('Count upto which multiplication to be done: '))
+    number=range(1,count+1)
+    for item in number:
+        res=item*17
+        print(item,'*','17','=',res)
+
+# Main starts from here
+print_mtable17()
