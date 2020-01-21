38d9c25599bc3b7675bfe4c1f9e05d9647c328f1
vijitha-damodaran <59793003+vijitha-damodaran@users.noreply.github.com>
2020-01-21 11:50:06 +0530

    Update mul_tab_for _any_number

--- mul_table_for_any_num.py
+++ mul_table_for_any_num.py
@@ -1,7 +1,29 @@
-#Multiplication table of a number using for
-count = int(input('Count upto which multiplication to be done: '))
-number=int(input('NUmber whose mul_table is required: '))
-#number=range(1,count+1)
-for item in range(1,count+1):
-    res=item*number
-    print(item,'*',number,'=',res)
+"""
+Prints Multiplication table of a number upto the count
+"""
+
+def get_number():
+    '''
+    This function is for getting the number from the user
+    '''
+    num=int(input('NUmber whose mul_table is required: '))
+    return num
+
+def print_mtable(num):
+    '''
+    This function prints the multiplication table
+    '''
+    count = int(input('Count upto which multiplication to be done: '))
+    for item in range(1,count+1):
+        res=item*num
+        print(item,'*',num,'=',res)
+
+def main():
+    '''
+    Main function
+    '''
+    number=get_number()
+    print_mtable(number)
+
+#Main program starts from here
+main()
