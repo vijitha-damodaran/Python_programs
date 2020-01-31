2e34df3fb62dbd263a6e44d2857c8c500e2c62cc
vijitha-damodaran <59793003+vijitha-damodaran@users.noreply.github.com>
2020-01-31 12:15:42 +0530

    Update sphere_cylinder.py

--- sphere_cylinder.py
+++ sphere_cylinder.py
@@ -1,28 +1,34 @@
 """
-A melting furnace converts a hollow sphere into a solid cylinder of 3cm diameter with a 5% loss. 
+A melting furnace converts a hollow sphere into a solid cylinder
+of 3cm diameter with a 5% loss. 
 Write a program that finds out the length of the solid cylinder when
  the inner and external radii of the hollow sphere are known
 
 Volume of sphere = 4/3 * pi * r * r * r
 Volume of cylinder = pi * r * r * height
 """
-def get_radius_ext():
-    res=int(sys.argv[1])
+
+def volume_sphere(r_ext,r_int):
+    res=4*math.pi*(r_ext**3-r_int**3)/3
     return res
 
-def get_radius_inner():
-    res=int(sys.argv[2])
+def volume_sphere_with_loss(vol,loss):
+    res=vol*95/100
     return res
 
-def height_cylindr(r_ext,r_int,r_cyl):
-    res=4*(r_ext**3-r_int**3)/(3*r_cyl**2)
+def height_cylinder(vol,radi):
+    res=vol/(math.pi*(radi**2))
     return res
 
 #main pgm starts from here
 if __name__ == "__main__":
     import sys
+    import math
     radii_cyl=3
-    radii_ext=get_radius_ext()
-    radii_int=get_radius_inner()
-    height=height_cylindr(radii_ext,radii_int,radii_cyl)
-    print('Height of cylinder=',height)
+    loss_perc=5
+    radii_ext=int(sys.argv[1])
+    radii_int=int(sys.argv[2])
+    vol_sphere=volume_sphere(radii_ext,radii_int)
+    vol_sphere_with_loss=volume_sphere_with_loss(vol_sphere,loss_perc)
+    height_cyl=height_cylinder(vol_sphere_with_loss,radii_cyl)
+    print('Height of cylinder=',height_cyl)
